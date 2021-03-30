99645c60a7c050c651d500de839afb6239932aec
Ludvig Lundgren <hello@ludviglundgren.se>
2021-03-30 19:14:22 +0200

    importer: wip

---
+++ pkg/qbittorrent/fastresume.go
@@ -0,0 +1,193 @@
+package qbittorrent
+
+import (
+	"bufio"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/ludviglundgren/qbittorrent-cli/pkg/torrent"
+
+	"github.com/zeebo/bencode"
+)
+
+type Fastresume struct {
+	ActiveTime        int64    `bencode:"active_time"`
+	AddedTime         int64    `bencode:"added_time"`
+	Allocation        string   `bencode:"allocation"`
+	ApplyIpFilter     int64    `bencode:"apply_ip_filter"`
+	AutoManaged       int64    `bencode:"auto_managed"`
+	CompletedTime     int64    `bencode:"completed_time"`
+	DisableDHT        int64    `bencode:"disable_dht"`
+	DisableLSD        int64    `bencode:"disable_lsd"`
+	DisablePEX        int64    `bencode:"disable_pex"`
+	DownloadRateLimit int64    `bencode:"download_rate_limit"`
+	FileFormat        string   `bencode:"file-format"`
+	FileVersion       int64    `bencode:"file-version"`
+	FilePriority      []int    `bencode:"file_priority"`
+	FinishedTime      int64    `bencode:"finished_time"`
+	HttpSeeds         []string `bencode:"httpseeds"`
+	//InfoHash          string   `bencode:"info-hash"`
+	InfoHash          []byte `bencode:"info-hash"`
+	LastDownload      int64  `bencode:"last_download"`
+	LastSeenComplete  int64  `bencode:"last_seen_complete"`
+	LastUpload        int64  `bencode:"last_upload"`
+	LibTorrentVersion string `bencode:"libtorrent-version"`
+	MaxConnections    int64  `bencode:"max_connections"`
+	MaxUploads        int64  `bencode:"max_uploads"`
+	NumComplete       int64  `bencode:"num_complete"`
+	NumDownloaded     int64  `bencode:"num_downloaded"`
+	NumIncomplete     int64  `bencode:"num_incomplete"`
+	Paused            int64  `bencode:"paused"`
+	//Pieces                    []byte                 `bencode:"pieces"`
+	Pieces                    string                 `bencode:"pieces"`
+	PiecePriority             []byte                 `bencode:"piece_priority"`
+	Peers                     string                 `bencode:"peers"`
+	Peers6                    string                 `bencode:"peers6"`
+	QbtCategory               string                 `bencode:"qBt-category"`
+	QbtContentLayout          string                 `bencode:"qBt-contentLayout"`
+	QbtHasRootFolder          int64                  `bencode:"qBt-hasRootFolder"`
+	QbtFirstLastPiecePriority int64                  `bencode:"qBt-firstLastPiecePriority"`
+	QbtName                   string                 `bencode:"qBt-name"`
+	QbtRatioLimit             int64                  `bencode:"qBt-ratioLimit"`
+	QbtSavePath               string                 `bencode:"qBt-savePath"`
+	QbtSeedStatus             int64                  `bencode:"qBt-seedStatus"`
+	QbtSeedingTimeLimit       int64                  `bencode:"qBt-seedingTimeLimit"`
+	QbtTags                   []string               `bencode:"qBt-tags"`
+	SavePath                  string                 `bencode:"save_path"`
+	SeedMode                  int64                  `bencode:"seed_mode"`
+	SeedingTime               int64                  `bencode:"seeding_time"`
+	SequentialDownload        int64                  `bencode:"sequential_download"`
+	ShareMode                 int64                  `bencode:"share_mode"`
+	StopWhenReady             int64                  `bencode:"stop_when_ready"`
+	SuperSeeding              int64                  `bencode:"super_seeding"`
+	TotalDownloaded           int64                  `bencode:"total_downloaded"`
+	TotalUploaded             int64                  `bencode:"total_uploaded"`
+	Trackers                  [][]string             `bencode:"trackers"`
+	UploadMode                int64                  `bencode:"upload_mode"`
+	UploadRateLimit           int64                  `bencode:"upload_rate_limit"`
+	UrlList                   []string               `bencode:"url-list"`
+	Unfinished                *[]interface{}         `bencode:"unfinished,omitempty"`
+	WithoutLabels             bool                   `bencode:"-"`
+	WithoutTags               bool                   `bencode:"-"`
+	HasFiles                  bool                   `bencode:"-"`
+	TorrentFilePath           string                 `bencode:"-"`
+	TorrentFile               map[string]interface{} `bencode:"-"`
+	Path                      string                 `bencode:"-"`
+	fileSizes                 int64                  `bencode:"-"`
+	sizeAndPrio               [][]int64              `bencode:"-"`
+	torrentFileList           []string               `bencode:"-"`
+	NumPieces                 int64                  `bencode:"-"`
+	PieceLength               int64                  `bencode:"-"`
+	MappedFiles               []string               `bencode:"mapped_files,omitempty"`
+	//MappedFiles []string `bencode:"-"`
+	//Replace             []replace.Replace      `bencode:"-"`
+	Separator string `bencode:"-"`
+}
+
+// Encode fastresume file
+func (fr *Fastresume) Encode(path string) error {
+	_, err := os.Create(path)
+	if err != nil {
+		log.Printf("os create error: %v", err)
+		return err
+	}
+
+	file, err := os.OpenFile(path, os.O_WRONLY, 0666)
+	if err != nil {
+		log.Printf("os open file error: %v", err)
+		return err
+	}
+
+	defer file.Close()
+
+	bufferedWriter := bufio.NewWriter(file)
+	enc := bencode.NewEncoder(bufferedWriter)
+	if err := enc.Encode(fr); err != nil {
+		log.Printf("encode error: %v", err)
+		return err
+	}
+
+	bufferedWriter.Flush()
+	return nil
+}
+
+// ConvertFilePriority for each file set priority
+func (fr *Fastresume) ConvertFilePriority(files []torrent.TorrentInfoFile) {
+	var newPrioList []int
+
+	// 0 Do not download, 1 Normal, 2 High
+	for i := 0; i < len(files); i++ {
+		newPrioList = append(newPrioList, 1)
+	}
+
+	fr.FilePriority = newPrioList
+}
+
+func (fr *Fastresume) FillPieces() {
+	//var newPieces = []string{}
+	var pieces = make([]byte, 0, fr.NumPieces)
+	//nchr, _ := strconv.Atoi(chr)
+
+	for i := int64(0); i < fr.NumPieces; i++ {
+		//pieces = append(pieces, byte(nchr))
+		pieces = append(pieces, byte(1))
+		//newPieces = append(newPieces, "\u0001")
+	}
+	fr.Pieces = string(pieces)
+
+	//ret := strings.Join(newPieces, "")
+}
+
+func (fr *Fastresume) FillWholePieces() {
+	var pieces = make([]byte, 0, fr.NumPieces)
+	for i := int64(0); i < fr.NumPieces; i++ {
+		pieces = append(pieces, byte(1))
+	}
+	fr.Pieces = string(pieces)
+}
+
+func (fr *Fastresume) FillWholePieces2() {
+	var pieces = make([]string, 0, fr.NumPieces)
+	for i := int64(0); i < fr.NumPieces; i++ {
+		pieces = append(pieces, "\x01")
+	}
+	//fr.Pieces = string(pieces)
+	fr.Pieces = strings.Join(pieces, "")
+}
+
+//func (newstructure *NewTorrentStructure) FillWholePieces(chr string) []byte {
+//	var newpieces = make([]byte, 0, newstructure.NumPieces)
+//	nchr, _ := strconv.Atoi(chr)
+//	for i := int64(0); i < newstructure.NumPieces; i++ {
+//		newpieces = append(newpieces, byte(nchr))
+//	}
+//	return newpieces
+//}
+
+func (fr *Fastresume) GetInfoHash() (hash string) {
+	torInfo, _ := bencode.EncodeString(fr.TorrentFile["info"].(map[string]interface{}))
+	h := sha1.New()
+	io.WriteString(h, torInfo)
+
+	hash = fmt.Sprintf("<hex>% X</hex>", h.Sum(nil))
+	return
+}
+func (fr *Fastresume) GetInfoHashSHA1() (hash []byte) {
+	torInfo, _ := bencode.EncodeString(fr.TorrentFile["info"].(map[string]interface{}))
+	h := sha1.New()
+	_, _ = h.Write([]byte(torInfo))
+	//io.WriteString(h, torInfo)
+
+	ab := h.Sum(nil)
+	return ab
+	//fmt.Println(ab)
+	//
+	//a := hex.EncodeToString(h.Sum(nil))
+	//
+	////hash = fmt.Sprintf("<hex>% X</hex>", h.Sum(nil))
+	//return a
+}
---
+++ pkg/torrent/torrent.go
@@ -0,0 +1,94 @@
+package torrent
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/zeebo/bencode"
+)
+
+// TorrentInfo torrent meta info
+type TorrentInfo struct {
+	Announce     string     `bencode:"announce"`
+	AnnounceList [][]string `bencode:"announce-list"`
+	CreatedBy    string     `bencode:"created by"`
+	CreationDate int64      `bencode:"creation date"`
+	Info         struct {
+		Length      int64             `bencode:"length"`
+		Name        string            `bencode:"name"`
+		PieceLength int64             `bencode:"piece length"`
+		Pieces      string            `bencode:"pieces"`
+		Private     bool              `bencode:"private"`
+		Files       []TorrentInfoFile `bencode:"files"`
+	} `bencode:"info"`
+	UrlList []string `bencode:"url-list"`
+}
+
+type TorrentInfoFile struct {
+	Length int64    `bencode:"length"`
+	Path   []string `bencode:"path"`
+}
+
+func Decode(path string) (*TorrentInfo, error) {
+	dat, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var torrent TorrentInfo
+	if err := bencode.DecodeBytes(dat, &torrent); err != nil {
+		return nil, err
+	}
+	return &torrent, nil
+}
+
+func OpenDecodeRaw(path string) (map[string]interface{}, error) {
+	dat, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var torrent map[string]interface{}
+	if err := bencode.DecodeBytes(dat, &torrent); err != nil {
+		return nil, err
+	}
+	return torrent, nil
+}
+
+func CopyFile(src string, dst string) error {
+	originalFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer originalFile.Close()
+	newFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+	if _, err := io.Copy(newFile, originalFile); err != nil {
+		return err
+	}
+	if err := newFile.Sync(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func CalculateInfoHash(torrent map[string]interface{}) (hash string) {
+	t, _ := bencode.EncodeString(torrent["info"])
+	h := sha1.New()
+	io.WriteString(h, t)
+	hash = hex.EncodeToString(h.Sum(nil))
+	return hash
+}
+
+func GetName(torrent map[string]interface{}) (name string) {
+	info, _ := torrent["info"].(map[string]interface{})
+	//info, _ := bencode.EncodeString(torrent["info"])
+	//h := sha1.New()
+	//io.WriteString(h, t)
+	//hash = hex.EncodeToString(h.Sum(nil))
+	return info["name"].(string)
+}
