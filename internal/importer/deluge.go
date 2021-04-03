0418a6b6bf2dd22022ba217b3439d4f2d5013869
Ludvig Lundgren <hello@ludviglundgren.se>
2021-04-03 22:12:57 +0200

    refactor: remove unused code

--- internal/importer/deluge.go
+++ internal/importer/deluge.go
@@ -1,7 +1,6 @@
 package importer
 
 import (
-	"bufio"
 	"io"
 	"io/ioutil"
 	"log"
@@ -151,42 +150,6 @@ func decodeFastresumeFile(path string) (map[string]interface{}, error) {
 	return torrent, nil
 }
 
-func decodeTorrentFile(path string) (map[string]interface{}, error) {
-	dat, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-	var torrent map[string]interface{}
-	if err := bencode.DecodeBytes(dat, &torrent); err != nil {
-		return nil, err
-	}
-	return torrent, nil
-}
-
-func encodeFastResumeFile(path string, newStructure *NewFastResumeFile) error {
-
-	_, err2 := os.Create(path)
-	if err2 != nil {
-		log.Printf("os create error: %v", err2)
-		return err2
-	}
-
-	file, err := os.OpenFile(path, os.O_WRONLY, 0666)
-	if err != nil {
-		log.Printf("os open file error: %v", err)
-		return err
-	}
-	defer file.Close()
-	bufferedWriter := bufio.NewWriter(file)
-	enc := bencode.NewEncoder(bufferedWriter)
-	if err := enc.Encode(newStructure); err != nil {
-		log.Printf("encode error: %v", err)
-		return err
-	}
-	bufferedWriter.Flush()
-	return nil
-}
-
 func copyFile(src string, dst string) error {
 	originalFile, err := os.Open(src)
 	if err != nil {
@@ -205,72 +168,4 @@ func copyFile(src string, dst string) error {
 		return err
 	}
 	return nil
-}
-
-type NewFastResumeFile struct {
-	ActiveTime         int64     `bencode:"active_time"`
-	AddedTime          int64     `bencode:"added_time"`
-	AnnounceToDht      int64     `bencode:"announce_to_dht"`
-	AnnounceToLsd      int64     `bencode:"announce_to_lsd"`
-	AnnounceToTrackers int64     `bencode:"announce_to_trackers"`
-	AutoManaged        int64     `bencode:"auto_managed"`
-	BannedPeers        string    `bencode:"banned_peers"`
-	BannedPeers6       string    `bencode:"banned_peers6"`
-	BlockPerPiece      int64     `bencode:"blocks per piece"`
-	CompletedTime      int64     `bencode:"completed_time"`
-	DownloadRateLimit  int64     `bencode:"download_rate_limit"`
-	FileSizes          [][]int64 `bencode:"file sizes"`
-	FileFormat         string    `bencode:"file-format"`
-	FileVersion        int64     `bencode:"file-version"`
-	FilePriority       []int     `bencode:"file_priority"`
-	FinishedTime       int64     `bencode:"finished_time"`
-	InfoHash           string    `bencode:"info-hash"`
-	LastSeenComplete   int64     `bencode:"last_seen_complete"`
-	LibtorrentVersion  string    `bencode:"libtorrent-version"`
-	MaxConnections     int64     `bencode:"max_connections"`
-	MaxUploads         int64     `bencode:"max_uploads"`
-	NumDownloaded      int64     `bencode:"num_downloaded"`
-	NumIncomplete      int64     `bencode:"num_incomplete"`
-	MappedFiles        []string  `bencode:"mapped_files,omitempty"`
-	Paused             int64     `bencode:"paused"`
-	Peers              string    `bencode:"peers"`
-	Peers6             string    `bencode:"peers6"`
-	Pieces             []byte    `bencode:"pieces"`
-
-	QbtHasRootFolder    int64    `bencode:"qBt-hasRootFolder"`
-	QbtCategory         string   `bencode:"qBt-category,omitempty"`
-	QbtName             string   `bencode:"qBt-name"`
-	QbtQueuePosition    int      `bencode:"qBt-queuePosition"`
-	QbtRatioLimit       int64    `bencode:"qBt-ratioLimit"`
-	QbtSavePath         string   `bencode:"qBt-savePath"`
-	QbtSeedStatus       int64    `bencode:"qBt-seedStatus"`
-	QbtSeedingTimeLimit int64    `bencode:"qBt-seedingTimeLimit"`
-	QbtTags             []string `bencode:"qBt-tags"`
-	QbtTempPathDisabled int64    `bencode:"qBt-tempPathDisabled"`
-
-	SavePath           string         `bencode:"save_path"`
-	SeedMode           int64          `bencode:"seed_mode"`
-	SeedingTime        int64          `bencode:"seeding_time"`
-	SequentialDownload int64          `bencode:"sequential_download"`
-	SuperSeeding       int64          `bencode:"super_seeding"`
-	TotalDownloaded    int64          `bencode:"total_downloaded"`
-	TotalUploaded      int64          `bencode:"total_uploaded"`
-	Trackers           [][]string     `bencode:"trackers"`
-	UploadRateLimit    int64          `bencode:"upload_rate_limit"`
-	Unfinished         *[]interface{} `bencode:"unfinished,omitempty"`
-
-	hasFiles        bool
-	torrentFilePath string
-	torrentFile     map[string]interface{}
-	path            string
-	fileSizes       int64
-	sizeAndPrio     [][]int64
-	torrentFileList []string
-	nPieces         int64
-	pieceLength     int64
-	replace         []Replace
-}
-
-type Replace struct {
-	from, to string
 }
