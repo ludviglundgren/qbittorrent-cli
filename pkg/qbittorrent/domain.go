0fb5ce1c945862672bd49351222d56ea6e45b10f
Ludvig Lundgren <hello@ludviglundgren.se>
2023-05-31 22:36:30 +0200

    Update pkg/qbittorrent/domain.go
    Co-authored-by: soup <soup@r4tio.dev>

--- pkg/qbittorrent/domain.go
+++ pkg/qbittorrent/domain.go
@@ -39,8 +39,6 @@ type Torrent struct {
 	TimeActive         int          `json:"time_active"`
 	TotalSize          int          `json:"total_size"`
 	Tracker            string       `json:"tracker"`
-	TrackerMessage     string       `json:"msg"`
-	TrackerStatus      int          `json:"status"`
 	UpLimit            int          `json:"up_limit"`
 	Uploaded           int          `json:"uploaded"`
 	UploadedSession    int          `json:"uploaded_session"`
