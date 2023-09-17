924f094dc8c96398c79c7ef07c1556156e21164b
Ludvig Lundgren <hello@ludviglundgren.se>
2023-09-17 15:18:58 +0200

    feat(tags): list add delete

--- cmd/tag.go
+++ cmd/tag.go
@@ -1,8 +1,15 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"text/template"
 
+	"github.com/ludviglundgren/qbittorrent-cli/internal/config"
+
+	"github.com/autobrr/go-qbittorrent"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -30,12 +37,76 @@ func RunTagList() *cobra.Command {
 		Long:  "List tags.",
 	}
 
+	var (
+		output string
+	)
+
+	command.Flags().StringVar(&output, "output", "", "Print as [formatted text (default), json]")
+
 	command.RunE = func(cmd *cobra.Command, args []string) error {
-		fmt.Println("run tag list")
+		config.InitConfig()
+
+		qbtSettings := qbittorrent.Config{
+			Host:      config.Qbit.Addr,
+			Username:  config.Qbit.Login,
+			Password:  config.Qbit.Password,
+			BasicUser: config.Qbit.BasicUser,
+			BasicPass: config.Qbit.BasicPass,
+		}
+
+		qb := qbittorrent.NewClient(qbtSettings)
+
+		ctx := cmd.Context()
+
+		if err := qb.LoginCtx(ctx); err != nil {
+			fmt.Fprintf(os.Stderr, "could not login to qbit: %q\n", err)
+			os.Exit(1)
+		}
+
+		tags, err := qb.GetTagsCtx(ctx)
+		if err != nil {
+			log.Fatal("could not get tags")
+		}
+
+		if len(tags) == 0 {
+			fmt.Println("No tags found")
+			return nil
+		}
+
+		switch output {
+		case "json":
+			res, err := json.Marshal(tags)
+			if err != nil {
+				log.Fatalf("could not marshal tags, err: %q", err)
+			}
+
+			fmt.Println(string(res))
+
+		default:
+			printTagsList(tags)
+
+		}
 		return nil
 	}
 
 	return command
+}
+
+var tagItemTemplate = `{{ range .}}
+Name: {{.}}
+{{end}}
+`
+
+func printTagsList(tags []string) {
+	tmpl, err := template.New("tags-list").Parse(tagItemTemplate)
+	if err != nil {
+		log.Fatalf("error: %q", err)
+	}
+
+	err = tmpl.Execute(os.Stdout, tags)
+	if err != nil {
+		log.Fatalf("could not generate template: %q", err)
+	}
 }
 
 // RunTagAdd cmd to add tags
@@ -45,9 +116,10 @@ func RunTagAdd() *cobra.Command {
 	)
 
 	var command = &cobra.Command{
-		Use:   "add",
-		Short: "Add tags",
-		Long:  "Add tags",
+		Use:     "add",
+		Short:   "Add tags",
+		Long:    "Add tags",
+		Example: `  qbt tag add tag1`,
 		Args: func(cmd *cobra.Command, args []string) error {
 			if len(args) < 1 {
 				return errors.New("requires a tag as first argument")
@@ -59,7 +131,41 @@ func RunTagAdd() *cobra.Command {
 	command.Flags().BoolVar(&dry, "dry-run", false, "Run without doing anything")
 
 	command.RunE = func(cmd *cobra.Command, args []string) error {
-		fmt.Println("run tag add")
+		config.InitConfig()
+
+		qbtSettings := qbittorrent.Config{
+			Host:      config.Qbit.Addr,
+			Username:  config.Qbit.Login,
+			Password:  config.Qbit.Password,
+			BasicUser: config.Qbit.BasicUser,
+			BasicPass: config.Qbit.BasicPass,
+		}
+
+		qb := qbittorrent.NewClient(qbtSettings)
+
+		ctx := cmd.Context()
+
+		if err := qb.LoginCtx(ctx); err != nil {
+			fmt.Fprintf(os.Stderr, "could not login to qbit: %q\n", err)
+			os.Exit(1)
+		}
+
+		// args
+		// first arg is tag
+		tag := args[0]
+
+		if dry {
+			log.Printf("dry-run: successfully created tag: %s\n", tag)
+
+			return nil
+
+		} else {
+			if err := qb.CreateTagsCtx(ctx, args); err != nil {
+				log.Fatal("could not create tag")
+			}
+
+			log.Printf("successfully created tag: %s\n", tag)
+		}
 		return nil
 	}
 
@@ -73,9 +179,10 @@ func RunTagDelete() *cobra.Command {
 	)
 
 	var command = &cobra.Command{
-		Use:   "delete",
-		Short: "Delete tags",
-		Long:  "Delete tags.",
+		Use:     "delete",
+		Short:   "Delete tags",
+		Long:    "Delete tags.",
+		Example: `  qbt tag delete tag1`,
 		Args: func(cmd *cobra.Command, args []string) error {
 			if len(args) < 1 {
 				return errors.New("requires a tag as first argument")
@@ -87,7 +194,41 @@ func RunTagDelete() *cobra.Command {
 	command.Flags().BoolVar(&dry, "dry-run", false, "Run without doing anything")
 
 	command.RunE = func(cmd *cobra.Command, args []string) error {
-		fmt.Println("run tag delete")
+		config.InitConfig()
+
+		qbtSettings := qbittorrent.Config{
+			Host:      config.Qbit.Addr,
+			Username:  config.Qbit.Login,
+			Password:  config.Qbit.Password,
+			BasicUser: config.Qbit.BasicUser,
+			BasicPass: config.Qbit.BasicPass,
+		}
+
+		qb := qbittorrent.NewClient(qbtSettings)
+
+		ctx := cmd.Context()
+
+		if err := qb.LoginCtx(ctx); err != nil {
+			fmt.Fprintf(os.Stderr, "could not login to qbit: %q\n", err)
+			os.Exit(1)
+		}
+
+		// args
+		// first arg is tag
+		tag := args[0]
+
+		if dry {
+			log.Printf("dry-run: successfully deleted tag: %s\n", tag)
+
+			return nil
+
+		} else {
+			if err := qb.DeleteTagsCtx(ctx, []string{tag}); err != nil {
+				log.Fatal("could not delete tag")
+			}
+
+			log.Printf("successfully deleted tag: %s\n", tag)
+		}
 		return nil
 	}
 
